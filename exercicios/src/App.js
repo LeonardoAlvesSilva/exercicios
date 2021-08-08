65b3a8fa122ad1685d8d8b5f3182209e9124743e
Leonardo Alves <leonardoalvesalves09@gmail.com>
2021-08-08 20:16:11 -0300

    Aula 20

---
+++ exercicios/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {SafeAreaView} from 'react-native'
+
+import Primeiro from './components/Primeiro'
+import {Comp1 , Comp2} from './components/Multi'
+
+export default () => <SafeAreaView>
+    
+    <Comp2/>
+    <Comp1/>
+    <Primeiro></Primeiro>
+
+
+</SafeAreaView>
+
---
+++ exercicios/src/components/Multi.js
@@ -0,0 +1,18 @@
+import { func } from 'joi'
+import { exp } from 'prelude-ls'
+import React from 'react'
+import  {Text, } from 'react-native'
+
+function Comp() {
+    return <Text>Comp #Oficial </Text>
+}
+
+function Comp1() {
+    return <Text>Comp #01</Text>
+}
+
+function Comp2() {
+    return <Text>Comp #02</Text>
+}
+
+export {Comp2, Comp1}
